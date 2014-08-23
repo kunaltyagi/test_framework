d3de764edc930bd59af725f14483488005a0a8c7
kunaltyagi <tyagi.kunal@live.com>
2014-08-23 23:39:45 +0530

    Adding a setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nsiqcppstyle',
+      version='1.0',
+      description='Modified C++ style checker',
+      author='Kunal Tyagi',
+      author_email='ktyagi@live.in',
+      url='http://github.com/kunaltyagi',
+      packages=[''],
+     )
